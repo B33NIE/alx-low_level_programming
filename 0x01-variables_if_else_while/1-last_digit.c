8fa3134da4554ab2f8c84fcedec65581ed27e3d1
B33NIE <bkselebano@icloud.com>
2022-07-02 08:48:52 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,9 +12,8 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	s = n % 10;
         printf("Last digit of %d is ",n);
-	if (n>5)
+	if (n > 5)
 	{
 	printf("greater than 5");
 	}
@@ -22,14 +21,10 @@ int main(void)
         {
 	printf("0");
 	}
-	if (n < 6 && n!=0)
+	if (n < 6 && n != 0)
 	{
 	printf("less than 6 not 0");
-        }		
-	else if (s < 6)
-        {
-        printf("Last digit of %d is %d and is less than 6 and not 0\n", n, s);
-	}
+        }	
 printf("\n");
 	return (0);
 }
