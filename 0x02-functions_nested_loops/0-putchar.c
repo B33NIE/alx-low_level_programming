954560935b9d37293b23ef716951dd98cab61033
B33NIE <bkselebano@icloud.com>
2022-07-06 12:39:57 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /*
- * main - prints _putchar
- * description: print '_putchar' followed by a new line
+ * main - prints 
+ * description: print _putchar followed by a new line
  * Return: Always 0 (Success)
 */
 int main(void)
@@ -15,5 +15,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar("\n");
-return (0)
+return (0);
 }
