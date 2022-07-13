62d790b884ce86c5c1454b2e553f8559ad8a3112
B33NIE <bkselebano@icloud.com>
2022-07-13 09:08:55 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/*
+ * reset_to_98 -> a function that updates the value of a pointer
+ * @n: pointer to n is to be updated
+ *
+*/ 
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef main_h
+#def main_h
+int _putchar(char c);
+
+
+#endif
