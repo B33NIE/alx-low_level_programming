bb5e088512e8d756874b49a80fbc29f55b922f04
B33NIE <bkselebano@icloud.com>
2022-07-16 13:17:39 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+ * main - program that prints the alphabet in lower case
+ *
+ * Return 0
+ *
+*/
+
+int main(void)
+{
+	int n;
+	for (n = 97; n <= 122; ++n)
+		putchar(n);
+	putchar(10)
+return 0;
+}	
