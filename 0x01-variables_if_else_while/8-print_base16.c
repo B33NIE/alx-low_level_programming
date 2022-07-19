2e60edc581e0ea76f50722f0f4f2e07536342989
B33NIE <bkselebano@icloud.com>
2022-07-19 07:26:51 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/*
+ * main -  Entry point
+ *
+ * Description: program that prints all the numbers of base 16 in lowercase
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int n = 48;
+
+	int l = 'a';
+
+	while (n <= 57)
+	{
+		putchar(n);
+		n += 1;
+	}
+	while (l <= 'f')
+	{
+		putchar(l);
+		l++;
+	}
+	putchar('\n');
+return (0);
+}
